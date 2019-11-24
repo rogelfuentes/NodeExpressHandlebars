4dbc0c449fa2734702fe4ee0c0a35c6f5905db15
Rogel Fuentes <rogelfuentes@gmail.com>
2019-11-23 22:03:39 -0700

    .

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-### Schema
+DROP DATABASE IF EXISTS pizza_db;
+CREATE DATABASE pizza_db;
+USE pizza_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
+CREATE TABLE pizzas
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
